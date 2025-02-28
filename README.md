ea7d08e4aa9ccbeea6b45476b178b3d7b874b436
karanyadavv <karanyadav729@gmail.com>
2025-02-28 18:05:16 +0530

    nav bar astracted

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Small is a clone of medium
 
 ## Features
 
-- [ ] Authentication using Clerk
+- [x] Authentication using Clerk
 - [ ] Admin dashboard
 - [ ] User Feed without the need to login
 - [ ] Sorting of articles based on popularity and multiple factors
---
+++ components/ui/navbar.tsx
@@ -0,0 +1,51 @@
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarSeparator,
+  MenubarShortcut,
+  MenubarTrigger,
+} from "@/components/ui/menubar"
+
+import {
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { ModeToggle } from "./mode-toggle";
+
+export default function Navbar(){
+  return (
+    <Menubar className="p-8 flex justify-between items-center gap-8 m-4 font-[family-name:var(--font-geist-sans)]">
+          {/* <MenubarMenu>
+            <MenubarTrigger>Write</MenubarTrigger>
+          </MenubarMenu> */}
+          <Link href={"/"} className="cursir-pointer font-bold">
+            small
+          </Link>
+          <div className="flex items-center gap-4">
+
+          <Menubar>
+            <MenubarMenu>
+              <SignedOut>
+                <SignInButton mode="modal">
+                  <Button>Login</Button>
+                </SignInButton >
+              </SignedOut>
+            </MenubarMenu>
+            <MenubarMenu>
+              <SignedIn>
+                <UserButton />
+              </SignedIn>
+            </MenubarMenu>
+          </Menubar>
+          <ModeToggle />
+          </div>
+        </Menubar>
+  )
+}
+
